eb8c15e9a07cb874628bf238001ae705fde08ae5
Nad Flores <nadflores.tech@gmail.com>
2024-05-31 15:22:39 +0800

    Replace none-ls with confirm.nvim

---
+++ lua/nadflores/plugins/colorscheme.lua
@@ -0,0 +1,37 @@
+-- Gruvbox
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({})
+    vim.cmd([[
+      colorscheme gruvbox
+      hi CocUnusedHighlight cterm=underline gui=underline guifg=#6B6860
+    ]])
+  end,
+}
+
+-- Tokyonight
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function()
+--     vim.cmd([[
+--       colorscheme tokyonight
+--     ]])
+--   end,
+-- }
+
+-- Catppuccin
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd([[
+--       colorscheme catppuccin
+--     ]])
+--   end,
+-- }
